67de46e479539310af4a2b832e6614a88048b5be
thongbui123 <88702681+thongbui123@users.noreply.github.com>
2023-07-10 13:50:01 +0700

    Create EIPAINTING_ArtExhibition2.java
    Done

---
+++ src/EIPAINTING_ArtExhibition2.java
@@ -0,0 +1,129 @@
+import java.io.*;
+import java.util.*;;
+
+public class EIPAINTING_ArtExhibition2 {
+
+	public static void main(String[] args) {
+		int n = ni();
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < n; i++) {
+			int number = ni();
+			if (!map.containsKey(number))
+				map.put(number, 1);
+			else
+				map.put(number, map.get(number) + 1);
+		}
+		List<Integer> list = new ArrayList<Integer>(map.values());
+		Collections.sort(list);
+		int count = 0, cur = 0, next = 0;
+		for (int i = 0; i < list.size(); i++) {
+			if (i != list.size() - 1) {
+				cur = list.get(i);
+				next = list.get(i + 1);
+				if (next > cur)
+					count += cur;
+				else
+					count += next;
+			}
+		}
+		System.out.println(count);
+	}
+
+	static InputStream is = System.in;
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+}
