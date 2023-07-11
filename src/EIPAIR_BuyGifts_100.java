d24718c013f613bde975bebcd387bfac81f5018c
thongbui123 <88702681+thongbui123@users.noreply.github.com>
2023-07-11 11:00:44 +0700

    EIPAIR Done
    Done

---
+++ src/EIPAIR_BuyGifts_100.java
@@ -0,0 +1,127 @@
+import java.io.*;
+import java.util.*;;
+
+public class EIPAIR_BuyGifts_100 {
+
+	public static void main(String[] args) {
+		int t = ni();
+		while (t-- > 0) {
+			int n = ni(), count = 0;
+			Map<Integer, Integer> map = new Hashtable<Integer, Integer>();
+			for (int i = 0; i < n; i++) {
+				int num = ni();
+				if (!map.containsKey(num))
+					map.put(num, 1);
+				else
+					map.put(num, map.get(num) + 1);
+			}
+			List<Integer> list = new ArrayList<>(map.values());
+			for (Integer integer : list) {
+				count += tinhSoCachChon(integer);
+			}
+			System.out.println(count);
+		}
+	}
+
+	static int tinhSoCachChon(int amount) {
+		return amount * (amount - 1) / 2;
+	}
+
+	static InputStream is = System.in;
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+}
