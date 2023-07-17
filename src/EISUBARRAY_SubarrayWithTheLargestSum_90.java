50283fa925492b8794d045af231195bcf22413dc
thongbui123 <thong.bui.set17@eiu.edu.vn>
2023-07-17 13:06:16 +0700

    Update EISUBARRAY_SubarrayWithTheLargestSum_90.java

--- src/EISUBARRAY_SubarrayWithTheLargestSum_90.java
+++ src/EISUBARRAY_SubarrayWithTheLargestSum_90.java
@@ -9,7 +9,7 @@ public class EISUBARRAY_SubarrayWithTheLargestSum_90 {
 		for (int i = 0; i < t; i++) {
 			arr[i] = ni();
 		}
-		int max = arr[0];
+		int max = 0;
 		for (int i = 0; i < arr.length; i++) {
 			int sum = 0;
 			for (int j = i; j < arr.length; j++) {
