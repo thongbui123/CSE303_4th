eb7aaeaf9b8d95f5cd368d7da78fded54fa7a7dd
thongbui123 <88702681+thongbui123@users.noreply.github.com>
2023-08-22 10:09:23 +0700

    Update EIUDEPRE_Cach2.java

--- src/EIUDEPRE_Cach2.java
+++ src/EIUDEPRE_Cach2.java
@@ -1,134 +1,35 @@
-import java.io.*;
-import java.util.*;;
+import java.util.*;
 
 public class EIUDEPRE_Cach2 {
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int n = ni();
-		long before = nl();
-		long after = nl();
-		double res = 0;
-		
-		
-
+		int n = sc.nextInt();
+		double before = sc.nextDouble();
+		double after = sc.nextDouble();
+		double res = binarySearch(0, 1, before, after, n);
 		System.out.println(res);
+	}
 
+	static double binarySearch(double low, double high, double before, double after, int year) {
+		while (high - low > 0.000001) {
+			double mid = (high + low) / 2;
+			double price = calPrice(before, year, mid);
+			if (price > after) {
+				low = mid;
+			} else {
+				high = mid;
+			}
+		}
+		return low;
 	}
 	
-	static double calculate(double x, long before, int n) {
+	static double calPrice(double before, int n, double x) {
 		double price = before;
 		for (int i = 0; i < n; i++) {
 			double rate = x - x * i / n;
 			price = (price * (1 - rate));
 		}
 		return price;
-	}
-
-	static double binarySearch(double left, double right, long before, long after) {
-		while (left <= right) {
-			double mid = (left + right) / 2;
-			double price = calculate(mid, before, lenbuf);
-			if (price >= after) {
-			} 
-		}
-		return left;
-	}
-
-	static InputStream is = System.in;
-	static byte[] inbuf = new byte[1 << 24];
-	static int lenbuf = 0, ptrbuf = 0;
-
-	static int readByte() {
-		if (lenbuf == -1)
-			throw new InputMismatchException();
-		if (ptrbuf >= lenbuf) {
-			ptrbuf = 0;
-			try {
-				lenbuf = is.read(inbuf);
-			} catch (IOException e) {
-				throw new InputMismatchException();
-			}
-			if (lenbuf <= 0)
-				return -1;
-		}
-		return inbuf[ptrbuf++];
-	}
-
-	static boolean isSpaceChar(int c) {
-		return !(c >= 33 && c <= 126);
-	}
-
-	static int skip() {
-		int b;
-		while ((b = readByte()) != -1 && isSpaceChar(b))
-			;
-		return b;
-	}
-
-	static double nd() {
-		return Double.parseDouble(ns());
-	}
-
-	static char nc() {
-		return (char) skip();
-	}
-
-	static String ns() {
-		int b = skip();
-		StringBuilder sb = new StringBuilder();
-		while (!(isSpaceChar(b))) {
-			sb.appendCodePoint(b);
-			b = readByte();
-		}
-		return sb.toString();
-	}
-
-	static char[] ns(int n) {
-		char[] buf = new char[n];
-		int b = skip(), p = 0;
-		while (p < n && !(isSpaceChar(b))) {
-			buf[p++] = (char) b;
-			b = readByte();
-		}
-		return n == p ? buf : Arrays.copyOf(buf, p);
-	}
-
-	static int ni() {
-		int num = 0, b;
-		boolean minus = false;
-		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
-			;
-		if (b == '-') {
-			minus = true;
-			b = readByte();
-		}
-		while (true) {
-			if (b >= '0' && b <= '9') {
-				num = num * 10 + (b - '0');
-			} else {
-				return minus ? -num : num;
-			}
-			b = readByte();
-		}
-	}
-
-	static long nl() {
-		long num = 0;
-		int b;
-		boolean minus = false;
-		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
-			;
-		if (b == '-') {
-			minus = true;
-			b = readByte();
-		}
-		while (true) {
-			if (b >= '0' && b <= '9') {
-				num = num * 10 + (b - '0');
-			} else {
-				return minus ? -num : num;
-			}
-			b = readByte();
-		}
 	}
 }
